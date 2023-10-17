4aa7e67510f235e782663dd02f42e984d9eac084
Talha Javed Khan <talhakhan1297@yahoo.com>
2023-10-17 13:38:54 +0500

    Added Cart Bloc, Events and State

--- lib/presentation/cart/bloc/cart_event.dart
+++ lib/presentation/cart/bloc/cart_event.dart
@@ -6,3 +6,43 @@ sealed class CartEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class FetchCartEvent extends CartEvent {}
+
+class AddItemEvent extends CartEvent {
+  const AddItemEvent(this.item);
+
+  final CartItemModel item;
+
+  @override
+  List<Object> get props => [item];
+}
+
+class RemoveItemEvent extends CartEvent {
+  const RemoveItemEvent(this.id);
+
+  final int id;
+
+  @override
+  List<Object> get props => [id];
+}
+
+class IncrementItemEvent extends CartEvent {
+  const IncrementItemEvent(this.id);
+
+  final int id;
+
+  @override
+  List<Object> get props => [id];
+}
+
+class DecrementItemEvent extends CartEvent {
+  const DecrementItemEvent(this.id);
+
+  final int id;
+
+  @override
+  List<Object> get props => [id];
+}
+
+class ClearCartEvent extends CartEvent {}
--- lib/presentation/cart/bloc/cart_state.dart
+++ lib/presentation/cart/bloc/cart_state.dart
@@ -1,10 +1,47 @@
 part of 'cart_bloc.dart';
 
-sealed class CartState extends Equatable {
-  const CartState();
-  
-  @override
-  List<Object> get props => [];
-}
+class CartState extends Equatable {
+  const CartState({
+    this.getCartState = const APIState<Map<int, CartItemModel>>(),
+    this.addItemState = const APIState<void>(),
+    this.removeItemState = const APIState<void>(),
+    this.incrementItemState = const APIState<void>(),
+    this.decrementItemState = const APIState<void>(),
+    this.clearCartState = const APIState<void>(),
+  });
 
-final class CartInitial extends CartState {}
+  final APIState<Map<int, CartItemModel>> getCartState;
+  final APIState<void> addItemState;
+  final APIState<void> removeItemState;
+  final APIState<void> incrementItemState;
+  final APIState<void> decrementItemState;
+  final APIState<void> clearCartState;
+
+  CartState copyWith({
+    APIState<Map<int, CartItemModel>>? getCartState,
+    APIState<void>? addItemState,
+    APIState<void>? removeItemState,
+    APIState<void>? incrementItemState,
+    APIState<void>? decrementItemState,
+    APIState<void>? clearCartState,
+  }) {
+    return CartState(
+      getCartState: getCartState ?? this.getCartState,
+      addItemState: addItemState ?? this.addItemState,
+      removeItemState: removeItemState ?? this.removeItemState,
+      incrementItemState: incrementItemState ?? this.incrementItemState,
+      decrementItemState: decrementItemState ?? this.decrementItemState,
+      clearCartState: clearCartState ?? this.clearCartState,
+    );
+  }
+
+  @override
+  List<Object> get props => [
+        getCartState,
+        addItemState,
+        removeItemState,
+        incrementItemState,
+        decrementItemState,
+        clearCartState,
+      ];
+}
